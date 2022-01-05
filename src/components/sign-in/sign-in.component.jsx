934d6bcd32be24e8fa69bf7f4313bf58c0d90451
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-01-05 14:55:22 +0100

    implemenet sign-out, adding and fixing styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,7 +51,7 @@ class SignIn extends React.Component {
             label="email"
             required
           />
-          
+
           <FormInput
             name="password"
             type="password"
@@ -60,9 +60,12 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-          
-          <CustomButton type="submit"> Sign in </CustomButton>
-          <CustomButton onClick={signInWithGoogle}> Sign in with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
