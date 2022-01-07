fe888712249446894538037932414eab53318c1c
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-01-07 17:58:18 +0100

    cart-item reducer: azaz tételek kosárba rakása

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+
+  const { name, price, imageUrl } = item;
+
+  return(
   <div className="collection-item">
     <div
       className="image"
@@ -13,11 +19,16 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       }}
     ></div>
     <div className="collection-footer">
-        <span className="name">{ name }</span>
-        <span className="price">{ price }</span>
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
-    <CustomButton inverted> ADD to cart </CustomButton>
+    <CustomButton onClick={() => addItem(item)} inverted> ADD to cart </CustomButton>
   </div>
-);
+  )
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
