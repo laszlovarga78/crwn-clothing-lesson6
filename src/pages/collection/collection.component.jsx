9d5d5eca769e4677a8b06ccb85d5d090e5d99355
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-01-17 19:55:59 +0100

    collection routing és selector készítés annak a probléműnak a megoldásához, hogy az url az string, de az id, amit keresünk az szám, ezt kell összemappelni. (152. videó)

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import { connect } from 'react-redux';
+
 // ez jeleníti meg a tételt:
 import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
 // a match.params.collectionId paramétert használjuk majd
-const CollectionPage = ({ match }) => {
-  console.log(match);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className="collection-page">
       <h2>COLLECTION PAGE</h2>
@@ -15,4 +19,8 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,30 @@
 import { createSelector } from "reselect";
 
+/**
+ * Azért használjuk ezt az objektumot, mert az url paraméter (pl: /shop/hats) egy sztring, míg az id. amivel match-elni akarunk az egy szám. Ezért összemappeljük a szövegeket az id-kal
+ */
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+/**
+ * A selectCollection megkapja az string formátumú url paramétert és visszatér egy createSelector függvénnyel, ami egy "curried függvény", ami visszatér egy másik függvénnyel
+ * 
+ */
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
