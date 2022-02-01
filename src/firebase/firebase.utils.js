f470e874c23fdb7a48a24f3b6ed9af6c54333f8a
Laszlo Varga <laszlo.1.varga@gmail.com>
2022-02-01 15:10:58 +0100

    adatok lekérése a firestore-ból

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -80,6 +80,22 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     return await batch.commit();
 }
 
+// az egész snapshot lekérése
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+
+    console.log(transformedCollection);
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,47 @@
 import React from "react";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
+// az adatokat a firestore-ból vesszük
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
 /**
- * Azt akarjuk a routinggal megvalósítani, hogy csak az adott ketegória alatti termékek jelenjenek meg.
+ * Azt akarjuk a routinggal megvalósítani, hogy csak az adott kategória alatti termékek jelenjenek meg.
  * pl: /shop/hats : hozzá kell férünk a shop utáni url paraméterhez, hogy meghatározhassuk melyik kategória
  * termékeit akarjuk fetch-elni
  */
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    
-  </div>
-);
+
+// a firestore-os adatlekérés miatt alakítottuk át class komponensé
+class ShopPage extends React.Component {
+
+  unsubscribeFromSnapshot = null;
+  /* A snapshot lesz a snapshot reprezentációjá a collection tömbnek, amit a firestortól kapunk vissza. Ezt a componentDidMount-on belül fetcheljük */
+
+  componentDidMount() {
+    // ezzel megszerezzük a firestore-ból a collection-t
+    const collectionRef = firestore.collection('collections');
+
+    // a collectionRef-ből akarjuk megkapni az adatokat
+    collectionRef.onSnapshot(async snapshot => {
+      //console.log(snapshot);
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
